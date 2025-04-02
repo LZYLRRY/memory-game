a11ba0a7d777d60a8538505d01110941baa576d6
LZYLRRY <162039530+LZYLRRY@users.noreply.github.com>
2025-04-02 17:44:41 +0200

    Add axios fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import { useState } from "react";
 import Form from "./components/Form";
 import MemoryCard from "./components/MemoryCard";
+import axios from "axios";
+import { decodeEntity } from "html-entities";
 
 function App() {
   const [isGameOn, setIsGameOn] = useState(false);
+  const [emojisData, setEmojisData] = useState([]);
 
-  function startGame(e) {
+  console.log("emojis data", emojisData);
+
+  async function startGame(e) {
     e.preventDefault();
-    setIsGameOn(true);
+    try {
+      const response = await axios.get(
+        "https://emojihub.yurace.pro/api/all/category/animals-and-nature"
+      );
+
+      console.log(response.data);
+      const dataSample = response.data.slice(0, 5);
+
+      setEmojisData(dataSample);
+      setIsGameOn(true);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   }
 
   function turnCard() {
@@ -18,7 +35,7 @@ function App() {
     <main>
       <h1>Memory</h1>
       {!isGameOn && <Form handleSubmit={startGame} />}
-      {isGameOn && <MemoryCard handleClick={turnCard} />}
+      {isGameOn && <MemoryCard handleClick={turnCard} data={emojisData} />}
     </main>
   );
 }
