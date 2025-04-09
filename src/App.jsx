4c37102545e72aaab1d02c6bdb7e71d7684df850
LZYLRRY <162039530+LZYLRRY@users.noreply.github.com>
2025-04-09 21:16:22 +0200

    GET Random Emojis and duplicate and shuffle emoji array

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,6 @@ function App() {
   const [isGameOn, setIsGameOn] = useState(false);
   const [emojisData, setEmojisData] = useState([]);
 
-  console.log("emojis data", emojisData);
-
   async function startGame(e) {
     e.preventDefault();
     try {
@@ -18,13 +16,55 @@ function App() {
       );
 
       console.log(response.data);
-      const dataSample = response.data.slice(0, 5);
+      const dataSlice = getDataSlice(response.data);
+      const emojisArray = getEmojisArray(dataSlice);
 
-      setEmojisData(dataSample);
+      setEmojisData(emojisArray);
       setIsGameOn(true);
+      console.log("random numbers", getRandomIndices(response.data));
+      console.log("getDataSlice", getDataSlice(response.data));
     } catch (error) {
       console.error("Error fetching data:", error);
     }
+  }
+
+  function getDataSlice(data) {
+    const randomIndices = getRandomIndices(data);
+    const dataSlice = randomIndices.map((number) => data[number]);
+
+    return dataSlice;
+    // ALSO POSSIBLE ON ONE LINE, BIT LESS READABLE
+    // return getRandomIndices(data).map((number) => data[number]);
+  }
+
+  function getRandomIndices(data) {
+    const randomIndicesArray = [];
+
+    for (let i = 0; i < 5; i++) {
+      const randomNum = Math.floor(Math.random() * data.length);
+      if (!randomIndicesArray.includes(randomNum)) {
+        randomIndicesArray.push(randomNum);
+      } else {
+        i--;
+      }
+    }
+
+    return randomIndicesArray;
+  }
+
+  function getEmojisArray(data) {
+    const pairedEmojisArray = [...data, ...data];
+
+    //Fisher-Yates shuffle
+    for (let i = pairedEmojisArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [pairedEmojisArray[i], pairedEmojisArray[j]] = [
+        pairedEmojisArray[j],
+        pairedEmojisArray[i],
+      ];
+    }
+
+    return pairedEmojisArray;
   }
 
   function turnCard() {
